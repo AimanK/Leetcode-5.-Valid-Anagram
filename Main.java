c8b2cfb4e40d3001ababd8ff5b03403f5da43cdc
Aiman Kayad <Akaya005@fiu.edu>
2022-08-17 14:10:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,6 +22,8 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
     - We then use the Arrays.sort() method to sort these char arrays.
     - Create a for loop, iterate until the length of one of the strings (we already checked), and then do a simple if statement within that compares if s_chars[i] != t_chars[i], return false.
     - If end of program reached successfully, return true.
+    
+    BIG TIP: Create two char arrays to represent each string, they are easy to sort using Arrays.sort, afterwards, iterate through to the length of one of the arrays to compare each letter together.
     **/
     
   }
